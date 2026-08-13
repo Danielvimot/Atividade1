77d2bfb8ec020dc3ed8c6e24a5e63ca1b9621ed3
Daniel Viana Mota <02749669138@ifb.local>
2026-08-13 14:44:45 -0300

    Adicionado projeto random versão 2

---
+++ random2.py
@@ -0,0 +1,9 @@
+import random
+
+# Lista com as suas opções de decisão
+respostas = ["Sim", "Não", "Com certeza não", "Esquece isso aí meu", "Vai dá não"]
+
+# O programa escolhe uma resposta aleatoriamente
+decisao = random.choice(respostas)
+
+print(f"Decisão do destino: {decisao}")
